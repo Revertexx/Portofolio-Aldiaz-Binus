0a6459e4a6af82c25d6be25cf269b09570a32f14
Aldiaz Kusuma Ramadhan <aldiazkr@Aldiazs-MacBook-Air.local>
2025-10-20 21:59:11 +0700

    netlify?

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -111,7 +111,7 @@ import {
             >
               <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
   
-              <form className="space-y-6">
+              <form className="space-y-6" netlify>
                 <div>
                   <label
                     htmlFor="name"
